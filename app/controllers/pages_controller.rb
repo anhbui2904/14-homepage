7dd44fff044d2c470f0c6153755120523d72e663
NTQ <NTQ@DESKTOP-3QQMBK5>
2019-11-27 16:32:23 +0700

    first commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,10 @@
+class PagesController < ApplicationController
+  def about
+  end
+
+  def location
+  end
+  def home
+  	
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  resources :xxxes
+  resources :courses
+  root 'pages#home'
+  get 'pages/about'
+  get 'pages/location'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
